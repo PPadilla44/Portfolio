83defd619dccf8969c863e882b79b10f4f010872
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-09-13 17:08:06 -0700

    finished Navbar (needs scroll to)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+
 // Color pallete
 // https://colorhunt.co/palette/ffe6e6ff2626bd1616000000
 
 function App() {
+
+  const [blur, setBlur] = useState(false);
+
   return (
     <div className="App">
-      <Nav/>
+      { blur && <div className={"blurBackground"} style={{ opacity: '0.85', background: 'hsl(0, calc(var(--saturation-factor, 1) * 0%), 0%)'}}/>}
+      <Nav setBlur={setBlur}/>
       <main className="d-flex flex-column align-items-center">
         <About/>
         <hr />
